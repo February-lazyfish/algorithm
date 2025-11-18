a5434167c2945ff2ba1c6c2642035963640798eb
February-lazyfish <lazyfish11064593@gmail.com>
2025-11-18 17:10:31 +0800

    selectionSort

--- Array/Kmptest.cc
+++ Array/Kmptest.cc
@@ -1,19 +1,21 @@
-#include <cstddef>
 #include <type_traits>
 #include <vector>
 #include <string>
 #include <iostream>
-using std::vector;
 using std::cout;
 using std::endl;
+using std::vector;
 using std::string;
 
+
 vector<int> build_lps(const string &pat){
-	int m=pat.size();
+	int m = pat.size();
 	vector<int> lps(m,0);
 	if(m==0) return lps;
+
 	int len=0;
 	int i=1;
+
 	while(i<m){
 		if(pat[i]==pat[len]){
 			len++;
@@ -27,24 +29,24 @@ vector<int> build_lps(const string &pat){
 				i++;
 			}
 		}
+
 	}
 	return lps;
 }
 
-vector<int> kmp_search_all(const string &txt,const string &apt){
+vector<int> kmp_search_all(const string &txt,const string &pat){
 	vector<int> res;
 	int n=(int)txt.size();
-	int m=(int)apt.size();
+	int m=(int)pat.size();
 	if(m==0){
 		res.push_back(0);
 		return res;
 	}
-
-	vector<int> lps = build_lps(apt);
-	int i=0; //	txt索引
-	int j=0; //apt索引
+	vector<int> lps = build_lps(pat);
+	int i=0;
+	int j=0;
 	while(i<n){
-		if(txt[i]==apt[j]){
+		if(txt[i]==pat[j]){
 			i++;
 			j++;
 			if(j==m){
@@ -60,27 +62,32 @@ vector<int> kmp_search_all(const string &txt,const string &apt){
 		}
 	}
 	return res;
-
 }
 
 
 
-int main(){
-	string txt="abcxabcdabcdabcyabcdabcy";
-	string pat="abcdabcy";
-	vector<int> lps = build_lps(pat);
-	cout << "pattern" << pat<< "\nLPS";
-	for(int x:lps) cout<<x<<' ';
-	cout << "\n\n";
+int main(void)
+{
+		string txt = "abcxabcdabcdabcyabcdabcy";
+    string pat = "abcdabcy";
 
-	vector<int> matches = kmp_search_all(txt,pat);
-if (matches.empty()) {
+    // 1) 打印 lps 数组以便查看
+    vector<int> lps = build_lps(pat);
+    cout << "Pattern: " << pat << "\nLPS: "; 
+
+	for(int x:lps){
+		cout<<x<<' ';
+	}
+		cout<<endl;
+
+	vector<int> matches = kmp_search_all(txt, pat);
+    if (matches.empty()) {
         cout << "No matches found\n";
     } else {
         cout << "Matches found at indices: ";
-		for (int idx:matches){
-			cout<<idx<<' ';
-			cout<<"\n";
-		}
-		
-}}
+        for (int idx : matches) cout << idx << ' ';
+        cout << "\n";
+    }
+
+    return 0;
+}
---
+++ Linkedlist/SearchfromEndN.cc
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+
+// 链表节点
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+// 创建16个节点的链表
+ListNode* createList() {
+    ListNode* head = new ListNode(1);
+    ListNode* current = head;
+    
+    for (int i = 2; i <= 16; i++) {
+        current->next = new ListNode(i);
+        current = current->next;
+    }
+    return head;
+}
+
+// 双指针法查找倒数第k个节点
+int findKthFromEnd(ListNode* head, int k) {
+    if (head == nullptr || k <= 0) {
+        return -1; // 错误标识
+    }
+    
+    ListNode* fast = head;
+    ListNode* slow = head;
+    
+    // 快指针先走k步
+    for (int i = 0; i < k; i++) {
+        if (fast == nullptr) {
+            return -1; // k超过链表长度
+        }
+        fast = fast->next;
+    }
+    
+    // 两个指针同时前进，直到快指针到达末尾
+    while (fast != nullptr) {
+        fast = fast->next;
+        slow = slow->next;
+    }
+    
+    return slow->val;
+}
+
+// 打印链表
+void printList(ListNode* head) {
+    ListNode* current = head;
+    while (current != nullptr) {
+        cout << current->val;
+        if (current->next != nullptr) cout << " -> ";
+        current = current->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // 创建16个节点的链表
+    ListNode* head = createList();
+    
+    cout << "链表: ";
+    printList(head);
+    cout << endl;
+    
+    // 测试查找倒数第k个节点
+    int k = 3; // 查找倒数第3个节点
+    int result = findKthFromEnd(head, k);
+    
+    if (result != -1) {
+        cout << "倒数第 " << k << " 个节点的值是: " << result << endl;
+    } else {
+        cout << "查找失败，k值不合法或超过链表长度" << endl;
+    }
+    
+    // 测试其他k值
+    int testCases[] = {1, 5, 16};
+    for (int k : testCases) {
+        int result = findKthFromEnd(head, k);
+        cout << "倒数第 " << k << " 个节点的值是: " << result << endl;
+    }
+    
+    return 0;
+}
+
+
+/*fast 必须越界变成 null 的同时，slow 才能正好停在倒数第 k 个节点。
+为什么？
+因为 fast 比 slow 永远领先 k 个节点。
+链表向右移动了k次
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ * */
